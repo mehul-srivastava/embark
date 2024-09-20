bd5a8673b982c4127164f900f4b7aa913f2dfc11
Mehul Srivastava <mehulsrivastava1206@gmail.com>
2024-09-20 21:18:25 +0530

    shift contract template to recoil

--- app/levels/[id]/_components/Editor.tsx
+++ app/levels/[id]/_components/Editor.tsx
@@ -1,63 +1,16 @@
 import React, { useLayoutEffect } from "react";
 import hljs from "highlight.js";
-import "highlight.js/styles/stackoverflow-dark.css"; // tokyo-night-dark.css
+import "highlight.js/styles/stackoverflow-dark.css";
 import { useRecoilValue } from "recoil";
 import { codeAtom } from "@/store/atoms";
+
+// Another theme: tokyo-night-dark.css
 
 const Editor = () => {
   useLayoutEffect(() => hljs.highlightAll());
   hljs.registerLanguage("rust", require("highlight.js/lib/languages/rust"));
 
-  // const code = useRecoilValue(codeAtom)
-
-  const code = `use anchor_lang::prelude::*;
-declare_id!("PUBLIC_KEY_HERE");
-
-#[program]
-pub mod counter_contract {
-    use super::*;
-
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> { 
-        let account = &ctx.accounts.counter_account;
-
-        msg!("Counter account has been initialised! Current count: {}", account.count);
-        Ok(())
-    }
-
-    pub fn update(ctx: Context<Update>, count: u64) -> Result<()> {
-        let account = &mut ctx.accounts.counter_account;
-
-        account.count = counter.count + 1;
-
-        msg!("Counter has been updated! Current count: {}", account.count);
-        Ok(())
-    }
-}
-
-#[derive(Accounts)]
-pub struct Initialize<'info> {
-    #[account(mut)]
-    pub user: Signer<'info>,
-
-    #[account(
-        init,
-        payer = user,
-        space = 8 + CounterAccount::INIT_SPACE)
-    )]
-    pub counter_account: Account<'info, CounterAccount>,
-    pub system_program: Program<'info, System>,
-}
-
-#[derive(Accounts)]
-pub struct Update<'info> {
-    #[account(mut)]
-    pub counter_account: Account<'info, CounterAccount>,
-}
-
-#[account]
-pub struct CounterAccount {
-    count: u64,
-}`;
+  const code = useRecoilValue(codeAtom);
 
   return (
     <div>
